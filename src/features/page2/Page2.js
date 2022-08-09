b4945dbf4c4ed4c12bf1414a2993fcb3653caeb3
Yayah Zakiyah <yayahzakiyah95@gmail.com>
2022-08-09 14:12:58 +0700

    useLocation

--- src/features/page2/Page2.js
+++ src/features/page2/Page2.js
@@ -1,14 +1,17 @@
 import React, { useEffect, useState } from 'react'
 import { Link, Outlet, useNavigate, useParams } from 'react-router-dom';
 
-export  const productList = [
+export  const productListDb = [
     {id: '1', productName: 'Product 1', productInfo: 'Info 1'},
     {id: '2', productName: 'Product 2', productInfo: 'Info 2'},
     {id: '3', productName: 'Product 3', productInfo: 'Info 3'},
 ];
 
 export const Page2 = () => {  
-
+    const [productList, setProductList] = useState([])
+    useEffect(() => {
+        setProductList(productListDb)
+    })
    return (
     <>
         <h2>Page 2</h2>
@@ -17,7 +20,8 @@ export const Page2 = () => {
                 {productList.map((product) => {
                     return <Link className='item' 
                                 key={product.id}
-                                to={`/page2/${product.id}`}>
+                                to={`/page2/${product.id}`}
+                                state={{product:product}}>
                                 {product.productName}</Link>
                 })}
             </div>
--- src/features/page2/Page2Info.js
+++ src/features/page2/Page2Info.js
@@ -1,27 +1,14 @@
 import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { useLocation, useParams } from "react-router-dom";
 import {productList} from "./Page2";
 
 const Page2Info = ()=> {
-    const params = useParams();
-
-    const [product, setProduct] = useState({});
-    useEffect(() => {
-        const id = params.id;
-        for (let p of productList) {
-            if (p.id === id) {
-                setProduct(p);
-                break;
-            }
-        }
-    }, [params.id])
+    const {state: {product}}= useLocation();
     return(
        <div>
-            {params.id ? <div>
                 ID      : {product.id}
                 Name    : {product.productName}
                 Info    : {product.productInfo}
-                </div> : <></>}
         </div>
 
     )
